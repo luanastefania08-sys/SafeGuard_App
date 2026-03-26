77eae1b3c6396b354513a23fa2c7a6ad754b013c
luanastefania08-sys <luanastefania08@gmail.com>
2026-03-26 10:30:20 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/anti/estafa/MainActivity.kt
+++ android/app/src/main/kotlin/com/anti/estafa/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.anti.estafa
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.os.PowerManager
-import android.provider.Settings
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -16,26 +10,11 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "isIgnoringBatteryOptimizations" -> {
-                    val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-                    result.success(powerManager.isIgnoringBatteryOptimizations(packageName))
-                }
-                "requestIgnoreBatteryOptimizations" -> {
-                    try {
-                        val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-                        intent.data = Uri.parse("package:$packageName")
-                        startActivity(intent)
-                        result.success(true)
-                    } catch (e: Exception) {
-                        result.error("ERROR", e.message, null)
-                    }
-                }
-                else -> {
-                    result.notImplemented()
-                }
+            if (call.method == "isIgnoringBatteryOptimizations") {
+                result.success(true) 
+            } else {
+                result.notImplemented()
             }
         }
     }
